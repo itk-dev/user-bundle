b79c3c99dd7488fe1019e1be17de9367f20973ed
Lars Steen <lats@aarhus.dk>
2019-10-16 14:37:20 +0200

    Adding bundle file

---
+++ src/ItkDevUserBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of itk-dev/user-bundle.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace ItkDev\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ItkDevUserBundle extends Bundle
+{
+
+}
